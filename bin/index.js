cfafc771bec3407d5570e7dc601cc37c15994573
mattdesl <dave.des@gmail.com>
2015-11-19 15:05:19 -0500

    ensure directory

--- bin/index.js
+++ bin/index.js
@@ -3,11 +3,18 @@ var argv = require('minimist')(process.argv.slice(2))
 var color = require('term-color')
 var path = require('path')
 var escapeStr = require('jsesc')
+var fs = require('fs')
 
 var findBroken = require('../')
 
 var cwd = process.cwd()
 var dir = argv._[0] || cwd
+
+var stat = fs.statSync(dir)
+if (!stat.isDirectory()) {
+  throw new Error('Must specify a directory as first argument')
+}
+
 findBroken(dir, argv, function (result) {
   result.errors.forEach(function (err) {
     console.error(color.red('ERROR'), err.message)
